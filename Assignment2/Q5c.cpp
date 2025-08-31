8c5c555940bf88b450b0ece7e2f97d57dd3cad2b
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-08-31 16:06:21 +0530

    Update Q5c.cpp

--- Assignment2/Q5c.cpp
+++ Assignment2/Q5c.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int a[4][4]={3,2,6,7,0,6,6,8,0,0,2,4,0,0,0,1};
+    int a[4][4]={3,0,0,0,2,-1,0,0,1,-2,2,0,1,3,4,2};
     cout << "The given matrix is: " << endl;
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
