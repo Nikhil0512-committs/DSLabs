2d8cd94e03c6bfa425b55c09ebb017a9c3aa7d3b
Nikhil0512-committs <nikhilsharma5122006@gmail.com>
2025-11-02 18:31:36 +0000

    changesss

--- Assignment6/Q5.cpp
+++ Assignment6/Q5.cpp
@@ -3,54 +3,55 @@ using namespace std;
 
 struct Node {
     int data;
-    Node* next;
-};
+        Node* next;
+        };
 
-Node* head = nullptr;
+        Node* head = nullptr;
 
-void insert(int value) {
-    Node* newNode = new Node();
-    newNode->data = value;
-    newNode->next = nullptr;
-    if (!head) {
-        head = newNode;
-        return;
-    }
-    Node* temp = head;
-    while (temp->next)
-        temp = temp->next;
-    temp->next = newNode;
-}
+        void insert(int value) {
+            Node* newNode = new Node();
+                newNode->data = value;
+                    newNode->next = nullptr;
+                        if (!head) {
+                                head = newNode;
+                                        return;
+                                            }
+                                                Node* temp = head;
+                                                    while (temp->next)
+                                                            temp = temp->next;
+                                                                temp->next = newNode;
+                                                                }
 
-bool isCircular() {
-    if (!head) return false;
-    Node* temp = head->next;
-    while (temp && temp != head)
-        temp = temp->next;
-    return temp == head;
-}
+                                                                bool isCircular() {
+                                                                    if (!head) return false;
+                                                                        Node* temp = head->next;
+                                                                            while (temp && temp != head)
+                                                                                    temp = temp->next;
+                                                                                        return temp == head;
+                                                                                        }
 
-int main() {
-    insert(10);
-    insert(20);
-    insert(30);
-    insert(40);
-    cout << "Before making circular: ";
-    if (isCircular())
-        cout << "Circular Linked List" << endl;
-    else
-        cout << "Not a Circular Linked List" << endl;
+                                                                                        int main() {
+                                                                                            insert(10);
+                                                                                                insert(20);
+                                                                                                    insert(30);
+                                                                                                        insert(40);
+                                                                                                            cout << "Before making circular: ";
+                                                                                                                if (isCircular())
+                                                                                                                        cout << "Circular Linked List" << endl;
+                                                                                                                            else
+                                                                                                                                    cout << "Not a Circular Linked List" << endl;
 
-    Node* temp = head;
-    while (temp->next)
-        temp = temp->next;
-    temp->next = head;
+                                                                                                                                        Node* temp = head;
+                                                                                                                                            while (temp->next)
+                                                                                                                                                    temp = temp->next;
+                                                                                                                                                        temp->next = head;
 
-    cout << "After making circular: ";
-    if (isCircular())
-        cout << "Circular Linked List" << endl;
-    else
-        cout << "Not a Circular Linked List" << endl;
+                                                                                                                                                            cout << "After making circular: ";
+                                                                                                                                                                if (isCircular())
+                                                                                                                                                                        cout << "Circular Linked List" << endl;
+                                                                                                                                                                            else
+                                                                                                                                                                                    cout << "Not a Circular Linked List" << endl;
 
-    return 0;
-}
+                                                                                                                                                                                        return 0;
+                                                                                                                                                                                        }
+                                                                                                                                                                                        
